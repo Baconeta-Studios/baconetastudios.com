916b3c059486d12c297462975b174570b9717d32
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-09-12 17:54:59 +1200

    Refactor project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// Styling
+import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home.tsx";
+
+function App() {
+
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path={'/'} element={<Home />} />
+                <Route path={'/about'} element={<></>} />
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Intro />
+            <About />
+            <Footer />
+        </>
+    );
+}
