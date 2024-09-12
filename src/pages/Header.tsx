6fff1e9156b0a22f612bcd1902f95457abb3ab51
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-09-12 18:17:54 +1200

    Improve header

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,12 @@
+// React
+import {Link} from "react-router-dom";
+
 export default function Footer() {
     return (
         <header>
-            <p>Header text</p>
+            <Link to={"/"} >About Us</Link>
+            <Link to={"/contact"} >Contact Us</Link>
+            <Link to={"https://github.com/Baconeta-Studios/"} >Our Github</Link>
         </header>
     );
 }
