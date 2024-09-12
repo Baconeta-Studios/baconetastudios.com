33b0c172c69a74526365d81f54d5637a45be5231
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-09-12 18:16:52 +1200

    Improve imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+// React
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+// Components
+import Home from "./pages/home/Home.tsx";
 // Styling
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/home/Home.tsx";
 
 function App() {
 
