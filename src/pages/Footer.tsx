c78a736c839c8825525fae25e8ec8acc9594ac0f
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-09-12 18:00:53 +1200

    Add page stubs

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Footer text</p>
+        </footer>
+    );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,15 @@
+// Import global components.
+import Header from "../Header";
+import Footer from "../Footer";
+// Import page components.
+import Main from "./components/Main.tsx";
+import About from "./components/About.tsx";
+
 export default function Home() {
     return (
         <>
             <Header />
-            <Intro />
+            <Main />
             <About />
             <Footer />
         </>
