f63e3d4986f0e4129982b63b90d16b0005b40c6e
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-09-16 15:03:14 +1200

    Fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 // React
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // Components
 import Home from "./pages/home/Home.tsx";
 import ContactUs from "./pages/contact/ContactUs.tsx";
 
 function App() {
 
-  return (
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path={'/'} element={<Home />} />
-                <Route path={'/contact'} element={<ContactUs />} />
-            </Routes>
-        </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home/>}/>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/contact'} element={<ContactUs/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,5 +1,5 @@
 // React
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
