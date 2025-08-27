45ae3b5e8a8465208e87d2f8e77aaedcd5393abb
Josh Villgren <joshuavillgren@gmail.com>
2025-08-27 23:50:11 +1200

    fix logo and move to centre correctly

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,9 +1,10 @@
 // React
 import { Link } from "react-router-dom";
 
-export default function Footer() {
+export default function Header() {
     return (
         <header className={"flex-container"}>
+            <img className={"flex-item"} src="/baconeta-whitelogo.png" alt="baconeta studios" width={64} height={64} />
             <div className={"flex-row"}>
                 <Link to={"/"} className={"flex-item"} >Setting The Stage</Link>
                 <Link to={"/about"} className={"flex-item"} >About Us</Link>
