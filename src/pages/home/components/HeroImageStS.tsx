975b5e87cc9c60b6fa6610e1cf2821c7215caad3
Josh Villgren <joshuavillgren@gmail.com>
2025-08-28 00:03:49 +1200

    add hero image

--- src/pages/home/components/HeroImageStS.tsx
+++ src/pages/home/components/HeroImageStS.tsx
@@ -1,6 +1,7 @@
 export default function HeroImageStS() {
     return (
         <div>
+            <img className="hero-image" src="/sts_comic_panel.png"  alt="hero image for setting the stage"/>
         </div>
     );
 }
