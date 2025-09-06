24855f33d061cf312b38d7cf671daa36dd7db5f5
Josh Villgren <joshuavillgren@gmail.com>
2025-09-07 00:08:32 +1200

    add download link and setup hr

--- src/pages/home/components/Main.tsx
+++ src/pages/home/components/Main.tsx
@@ -2,8 +2,9 @@ export default function Main() {
     return (
         <div>
             <img src="/sts-title.png" alt="Setting The Stage" width="482" height="229"/>
-            <p>A musical puzzle adventure.
-                <br/>Give your feedback <a className="sts-link" href="https://forms.gle/3ezPYAAhuyHWZHjY9">here</a></p>
+            {/*<p>A musical puzzle adventure.*/}
+                {/*<br/>Give your feedback <a className="sts-link" href="https://forms.gle/3ezPYAAhuyHWZHjY9">here</a></p>*/}
+            <h2>Download Now</h2>
         </div>
     );
 }
