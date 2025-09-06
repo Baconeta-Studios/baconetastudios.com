6ade25ede042d54a42514cf24c20e6967ce055f5
Josh Villgren <joshuavillgren@gmail.com>
2025-09-07 00:52:16 +1200

    title screen update

--- src/pages/home/components/ComicPanelSts.tsx
+++ src/pages/home/components/ComicPanelSts.tsx
@@ -1,7 +1,7 @@
 export default function ComicPanelSts() {
     return (
         <div>
-            <img className="hero-image" src="/sts_comic_panel.png"  alt="hero image for setting the stage"/>
+            <img className="comic-image rounded-corners" src="/sts_comic_panel.png"  alt="hero image for setting the stage"/>
         </div>
     );
 }
