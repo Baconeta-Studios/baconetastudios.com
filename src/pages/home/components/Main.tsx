3a99a4abe955c2c946e6748d01cae4e329a707f7
Josh Villgren <joshuavillgren@gmail.com>
2025-09-07 00:58:56 +1200

    some more tidy ups

--- src/pages/home/components/Main.tsx
+++ src/pages/home/components/Main.tsx
@@ -4,7 +4,7 @@ export default function Main() {
             <div className={"hero"}>
                 <img src="/STS_Banner-dimmed-websitebanner.png" alt="Background" className="bg"/>
                 <img src="/sts-title.png" alt="Website Title" className="title" height="229"/>
-                {/*<img src="" alt="Setting The Stage" width="482" />*/}
+                <div className="overlay-text">a musical puzzle adventure</div>
             </div>
             {/*<p>A musical puzzle adventure.*/}
             {/*<br/>Give your feedback <a className="sts-link" href="https://forms.gle/3ezPYAAhuyHWZHjY9">here</a></p>*/}
