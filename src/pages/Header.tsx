b5565956ba2aa1571c2aae152799b839c03e7155
Josh Villgren <joshuavillgren@gmail.com>
2025-09-12 20:46:46 +1200

    studio icon margins

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header className={"flex-container"}>
-            <img className={"flex-item"} src="/baconeta-whitelogo.png" alt="baconeta studios" width={64} height={64} />
+            <img className={"flex-item studio-icon"} src="/baconeta-whitelogo.png" alt="baconeta studios" width={64} height={64} />
             <div className={"flex-row"}>
                 <Link to={"/"} className={"flex-item"} >Setting The Stage</Link>
                 <Link to={"/about"} className={"flex-item"} >About Us</Link>
