32c5547bc20c7097e3f901e434baddc86307eb64
Josh Villgren <joshuavillgren@gmail.com>
2025-09-17 21:26:09 +1200

    add badge to title and fix merge issues

--- src/pages/home/components/Main.tsx
+++ src/pages/home/components/Main.tsx
@@ -5,9 +5,11 @@ export default function Main() {
                 <img src="/STS_Banner-dimmed-websitebanner.png" alt="Background" className="bg"/>
                 <img src="/sts-title.png" alt="Website Title" className="title" height="229"/>
                 <div className="overlay-text">a musical puzzle adventure</div>
+
+                {/* Circle Badge */}
+                <div className={"hero-circle"}>GIVE US<br/>YOUR<br/>FEEDBACK!</div>
             </div>
-            {/*<p>A musical puzzle adventure.*/}
-            {/*<br/>Give your feedback <a className="sts-link" href="https://forms.gle/3ezPYAAhuyHWZHjY9">here</a></p>*/}
+
             <h2 className={"download-title"}>Download Now</h2>
         </div>
     );
