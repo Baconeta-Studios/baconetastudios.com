776112cff38f983bc1aa14c0ab94f91254ec8a4e
Josh Villgren <joshuavillgren@gmail.com>
2025-09-26 14:10:03 +1200

    Update StSMainContent.tsx

--- src/pages/home/components/StSMainContent.tsx
+++ src/pages/home/components/StSMainContent.tsx
@@ -13,8 +13,6 @@ export default function StSMainContent() {
             </div>
 
             <h2 className={"download-title"}>Download Now</h2>
-            <a href={"https://baconeta.itch.io/setting-the-stage-pc-demo"}><u>Download for PC</u></a>
-
         </div>
     );
 }
